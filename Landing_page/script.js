9999744aa308f3e288a25939fd7b5f63f2cdae56
RDHUNTERGITHUB <92819386+RDHUNTERGITHUB@users.noreply.github.com>
2022-09-15 08:28:31 -0400

    Assignment2
    Finished Assignment 2 and some bug fixes. However, still having trouble with grid.

--- Landing_page/script.js
+++ Landing_page/script.js
@@ -1,17 +1,34 @@
 let hour = new Date().getHours();
 console.log(hour);
 
-let dark_mode_color = "gray";
-let day_mode_color = "white";
-let dark_mode_opacity = 70%
-
 let background = document.querySelector('body');
-
-if(hour >= 18 || hour < 6){
-    background.style.backgroundColor = dark_mode_color;
-    background.style.opacity = dark_mode_opacity;
-
-}else{
-    background.style.backgroundColor = day_mode_color;
-    background.style.opacity = 1;
+if (hour>= 18 || hour < 6){
+    background.style.backgroundColor = 'grey';
+}else {
+    background.style.backgroundColor = 'yellow';
 }
+
+/*Assignment 2 */
+
+const d = new Date();
+let day = d.getDay();
+
+console.log(day);
+
+if (day == 1){
+    document.getElementById("h2").textContent = "Have a Marvelous Monday!"
+}else if(day == 2){
+    document.getElementById("h2").textContent = "Have a Terrific Tuesday!"
+}else if(day == 3){
+    document.getElementById("h2").textContent = "Have a Wonderful Wednesday!"
+}else if(day == 4){
+    document.getElementById("h2").textContent = "Have a Thoughtful Thursday!"
+}else if(day == 5){
+    document.getElementById("h2").textContent = "Have a Fabulous Friday!"
+}else if(day == 6){
+    document.getElementById("h2").textContent = "Have a Savvy Saturday!"
+}else if(day == 7){
+    document.getElementById("h2").textContent = "Have a Showy Sunday!"
+}
+
+
